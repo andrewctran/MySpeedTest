875e6e1ab7a0cb524fd913b20e4bb790d5611141
Andrew Tran <atran@princeton.edu>
2015-11-16 21:47:39 -0500

    [WIP] Adds packet model

--- app/src/main/java/com/num/model/Packet.java
+++ app/src/main/java/com/num/model/Packet.java
@@ -17,17 +17,65 @@ public class Packet {
         public static final int ACK = 0x10;
         public static final int URG = 0x20;
 
+        // TCP header segments
         private int srcPort;
         private int dstPort;
         private long seqNum;
         private long ackNum;
+        public byte offsetAndReserved;
+        public int headerLength;
+        public byte flags;
+        public int window;
+        public int checksum;
+        public int urgentPointer;
+        public byte[] optionsAndPadding;
 
         private TcpHeader(ByteBuffer byteBuffer) {
             this.srcPort = BitUtil.getUnsignedShort(byteBuffer.getShort());
             this.dstPort = BitUtil.getUnsignedShort(byteBuffer.getShort());
             this.seqNum = BitUtil.getUnsignedInt(byteBuffer.getInt());
             this.ackNum = BitUtil.getUnsignedInt(byteBuffer.getInt());
-            
+            this.offsetAndReserved = byteBuffer.get();
+            this.headerLength = (this.offsetAndReserved & 0xF0) >> 2;
+            this.flags = byteBuffer.get();
+            this.window = BitUtil.getUnsignedShort(byteBuffer.getShort());
+            this.checksum = BitUtil.getUnsignedShort(byteBuffer.getShort());
+            this.urgentPointer = BitUtil.getUnsignedShort(byteBuffer.getShort());
+
+            if (this.headerLength - TCP_HEADER_SIZE > 0) {
+                optionsAndPadding = new byte[this.headerLength - TCP_HEADER_SIZE];
+                byteBuffer.get(optionsAndPadding, 0, this.headerLength - TCP_HEADER_SIZE);
+            }
+        }
+
+        public boolean isFIN()
+        {
+            return (flags & FIN) == FIN;
+        }
+
+        public boolean isSYN()
+        {
+            return (flags & SYN) == SYN;
+        }
+
+        public boolean isRST()
+        {
+            return (flags & RST) == RST;
+        }
+
+        public boolean isPSH()
+        {
+            return (flags & PSH) == PSH;
+        }
+
+        public boolean isACK()
+        {
+            return (flags & ACK) == ACK;
+        }
+
+        public boolean isURG()
+        {
+            return (flags & URG) == URG;
         }
     }
 }
