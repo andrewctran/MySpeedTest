e42ac5b8968409e2acf213c23246e9699e8c4d29
Andrew Tran <atran@princeton.edu>
2015-11-23 21:04:55 -0500

    Changes application name on debug build; finishes TransmissionControlBlock

--- app/src/main/java/com/num/model/TransmissionControlBlock.java
+++ app/src/main/java/com/num/model/TransmissionControlBlock.java
@@ -7,17 +7,19 @@ import com.num.controller.utils.SynchronizedLruCache;
 import java.io.IOException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 import java.util.Map;
 
 public class TransmissionControlBlock {
     private static final int MAX_CACHE_SIZE = 50;
-//    private static LruCache<String, TransmissionControlBlock> cache =
-//            new SynchronizedLruCache<String, TransmissionControlBlock>(MAX_CACHE_SIZE, new SynchronizedLruCache.RemovalCallback() {
-//                @Override
-//                public void remove(Map.Entry<String, TransmissionControlBlock> eldest) {
-//                    eldest.getValue().closeChannel();
-//                }
-//            };
+    private static SynchronizedLruCache<String, TransmissionControlBlock> cache =
+            new SynchronizedLruCache<>(MAX_CACHE_SIZE,
+                    new SynchronizedLruCache.RemovalCallback<String, TransmissionControlBlock>() {
+        @Override
+        public void remove(Map.Entry<String, TransmissionControlBlock> eldest) {
+            eldest.getValue().closeChannel();
+        }
+    });
     public String ipPort;
     public long thisSeqNum;
     public long otherSeqNum;
@@ -39,15 +41,51 @@ public class TransmissionControlBlock {
         LAST_ACK,
     }
 
+    public TransmissionControlBlock(String ipPort, long thisSeqNum, long otherSeqNum, long thisAckNum, long otherAckNum,
+                                    Packet packet, SocketChannel socketChannel) {
+        this.ipPort = ipPort;
+        this.thisSeqNum = thisSeqNum;
+        this.otherSeqNum = otherSeqNum;
+        this.thisAckNum = thisAckNum;
+        this.otherSeqNum = otherAckNum;
+        this.packet = packet;
+        this.socketChannel = socketChannel;
+    }
+
+    public static void close(TransmissionControlBlock tcb) {
+        tcb.closeChannel();
+        synchronized (cache) {
+            cache.remove(tcb.ipPort);
+        }
+    }
+
+    public static void closeAll() {
+        synchronized (cache) {
+            Iterator<Map.Entry<String, TransmissionControlBlock>> iterator = cache.entrySet().iterator();
+            while (iterator.hasNext()) {
+                iterator.next().getValue().closeChannel();
+                iterator.remove();
+            }
+        }
+    }
+
     private void closeChannel() {
         try {
             socketChannel.close();
         } catch (IOException e) {
-
+            // Do nothing.
         }
     }
 
+    public static TransmissionControlBlock get(String key) {
+        synchronized (cache) {
+            return cache.get(key);
+        }
+    }
 
-
-
+    public static void put(String key, TransmissionControlBlock tcb) {
+        synchronized (cache) {
+            cache.put(key, tcb);
+        }
+    }
 }
